ebe839b88cd959f2863eb867c47fc97e5b881677
Dave Alden <dpa99c@gmail.com>
2015-04-29 10:30:26 +0100

    Initial version

---
+++ src/IsTablet.java
@@ -0,0 +1,72 @@
+/*
+   Copyright 2015 Dave Alden - http://www.workingedge.co.uk/dave/
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+/**
+ * Cordova/Phonegap plugin for Android to determine if current device is a tablet
+ * @author Dave Alden <dave@workingedge.co.uk>
+ */
+package uk.co.workingedge.phonegap.plugin.IsTablet;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaInterface;
+import org.apache.cordova.api.CordovaPlugin;
+
+
+public class IsTablet extends CordovaPlugin {
+	private static final String LOG_TAG = "IsTabletPlugin";
+
+
+	@Override
+	public boolean execute(String action, JSONArray args,
+			CallbackContext callbackContext) throws JSONException {
+
+		Log.d(LOG_TAG, "Plugin execute called - " + this.toString() );
+		result = isTabletDevice(callbackContext);
+		callbackContext.success();
+		return result;
+	}
+
+	private static boolean isTabletDevice(Context activityContext) {
+	        boolean device_large = ((activityContext.getResources().getConfiguration().screenLayout &
+	                Configuration.SCREENLAYOUT_SIZE_MASK) ==
+	                Configuration.SCREENLAYOUT_SIZE_LARGE);
+
+	        if (device_large) {
+	            DisplayMetrics metrics = new DisplayMetrics();
+	            Activity activity = (Activity) activityContext;
+	            activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+	            if (metrics.densityDpi == DisplayMetrics.DENSITY_DEFAULT
+	                    || metrics.densityDpi == DisplayMetrics.DENSITY_HIGH
+	                    || metrics.densityDpi == DisplayMetrics.DENSITY_MEDIUM
+	                    || metrics.densityDpi == DisplayMetrics.DENSITY_TV
+	                    || metrics.densityDpi == DisplayMetrics.DENSITY_XHIGH) {
+	                Log.d(LOG_TAG, "Is Tablet Device");
+	                return true;
+	            }
+	        }
+	        Log.d(LOG_TAG, "Is NOT Tablet Device");
+	        return false;
+    }
+
+}
