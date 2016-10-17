cb80afaf1d6041bdde71baa91ac3c1ae80ce256c
Dave Alden <dpa99c@gmail.com>
2016-10-17 09:09:31 +0100

    Rename typing files to use NPM package names instead of Github repo names. Reference type declaration in package.json.

--- phonegap-istablet-tests.ts
+++
@@ -1,4 +0,0 @@
-﻿/// <reference path="phonegap-istablet.d.ts" />
-
-let isTablet = window.isTablet();
-console.log("isTablet: "+isTablet);
--- uk.co.workingedge.phonegap.plugin.istablet.d.ts
+++ uk.co.workingedge.phonegap.plugin.istablet.d.ts
@@ -1,4 +1,4 @@
-﻿// Type definitions for phonegap-is-tablet v1.1.0
+﻿// Type definitions for uk.co.workingedge.phonegap.plugin.istablet@1.1.0
 // Project: https://github.com/dpa99c/phonegap-istablet
 // Definitions by: Dave Alden <https://github.com/dpa99c/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -7,7 +7,7 @@
 interface Window {
     /**
      * Indicates whether the current device is a tablet or a phone.
-     * @return {boolean} true if the device is a tablet; false if the device is a phone.
+     * True if the device is a tablet; false if the device is a phone.
      */
-	isTablet(): boolean;
+    isTablet: boolean;
 }
