941dfb690a329cb29d2ae3c06b4f99cbbdf522d0
Timon Orawski <timon@orawski.com>
2017-04-25 11:20:48 -0400

    better comments

--- src/windows/istabletproxy.js
+++ src/windows/istabletproxy.js
@@ -3,9 +3,11 @@ cordova.commandProxy.add("IsTablet", {
     try {
       var iAmTablet = false;
       var family = Windows.System.Profile.AnalyticsInfo.versionInfo.deviceFamily;
-      if (family == "Windows.Desktop") {
+      if (family == "Windows.Desktop" || family == "Windows.Team") {
+        // for the purposes of this plugin, anything that is not phone is really a tablet ...
         iAmTablet = true;
-        //(Windows.UI.ViewManagement.UIViewSettings.getForCurrentView().userInteractionMode != Windows.UI.ViewManagement.UserInteractionMode.mouse);
+        // if you really just care about mouse input instead:
+        // iAmTablet = (Windows.UI.ViewManagement.UIViewSettings.getForCurrentView().userInteractionMode != Windows.UI.ViewManagement.UserInteractionMode.mouse);
       }
       successCallback(iAmTablet);
     } catch(e) {
